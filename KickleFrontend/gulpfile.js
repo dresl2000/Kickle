6c9a383b7a9e5b2d46c311b90e3e80821aa3604d
Andreas Zimmerer <dresl2000@gmx.de>
2017-05-26 07:49:33 +0200

    added susy

---
+++ KickleFrontend/gulpfile.js
@@ -0,0 +1,65 @@
+var gulp  = require('gulp'),
+	sass   = require('gulp-sass');
+    gutil = require('gulp-util'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	noop = require('gulp-noop'),
+	sourcemaps = require('gulp-sourcemaps'),
+	ts = require('gulp-typescript');
+
+
+
+gulp.task('default', ['watch']);
+
+
+
+gulp.task('build-css', function() {
+return gulp.src('source/scss/**/*.scss')
+    .pipe(sourcemaps.init())  // Process the original sources
+      .pipe(sass({
+          outputStyle: 'compressed',
+          includePaths: ['node_modules/susy/sass']
+      }).on('error', sass.logError))
+    .pipe(sourcemaps.write()) // Add the map to modified source.
+    .pipe(gulp.dest('public/css'));
+});
+
+
+
+
+gulp.task('copyHtml', function() {  
+  gulp.src('source/**/*.html').pipe(gulp.dest('public'));
+});
+
+gulp.task('copyFonts', function() {  
+  gulp.src('source/fonts/**').pipe(gulp.dest('public/fonts'));
+});
+
+gulp.task('copyImages', function() {  
+  gulp.src('source/images/**').pipe(gulp.dest('public/images'));
+});
+
+gulp.task('compile-ts', function () {
+    return gulp.src('source/**/*.ts')
+        .pipe(ts({
+            noImplicitAny: true,
+            out: 'bundle.js'
+        }))
+        .pipe(gulp.dest('public/javascript'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('source/**/*.ts', ['compile-ts']);
+  gulp.watch('source/scss/**/*.scss', ['build-css']);
+  gulp.watch('source/**/*.html', ['copyHtml']);
+});
+
+gulp.task('build', function() {
+  gulp.start('build-css');
+  gulp.start('copyHtml');
+  gulp.start('copyFonts');
+  gulp.start('copyImages');
+  gulp.start('compile-ts');
+});
+
+
---
+++ KickleFrontend/readme.txt
@@ -0,0 +1,6 @@
+Structure:
+https://scotch.io/tutorials/angularjs-best-practices-directory-structure
+
+
+Gulp:
+https://scotch.io/tutorials/automate-your-tasks-easily-with-gulp-js
