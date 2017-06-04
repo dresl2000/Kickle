a6a45670da0acda636a885eea28649fecaa3289a
Andreas Zimmerer <dresl2000@gmx.de>
2017-06-04 14:40:57 +0200

    stepsservice

--- KickleFrontend/source/components/stepsOverview/stepsOverview.ts
+++ KickleFrontend/source/components/stepsOverview/stepsOverview.ts
@@ -1,9 +1,18 @@
 (function(){
 
-kickleApp.controller('stepsOverviewController', function(){
+kickleApp.controller('stepsOverviewController',['$log','stepsService', function($log: any, stepsService: any){
 
 
+	function init() {
+		$log.log('initializing stepsOverviewController');
+		
+		$log.log(stepsService.getDice(2));
+		$log.log(stepsService.rollStep(4));
+		
+	};
+	
 
-
-});
+	init();	
+		
+}]);
 })();
--- KickleFrontend/source/service/stepsService.ts
+++ KickleFrontend/source/service/stepsService.ts
@@ -2,36 +2,64 @@
 
 kickleApp.service('stepsService', ['$log', function($log: any){
 	
+	
 	class Step {
 		public stepNumber: number;
 		
-		var dice: Die[]
+		private dice : Die[];
+		
+		constructor(){
+			dice = new Array() as Array<Die>;
+			dice.push(new Die(2,6));
+		};		
 		
 		toString() : String{
 			
+			let str : String = ''; 
+			
+			for (let die of dice) {
+				
+				if(str.length > 0){
+					str += ' ' + die.toString();				
+				}
+				else{
+					str = die.toString(); 	
+				}				
+			}
+			
+			return str;
 		};
 		
 		roll(): number {
+			let result : number = 0; 
 			
+			for (let die of dice) {								
+				result += die.roll(); 							
+			}
+			
+			return result;
 		};
 	}	
 	
 	class Die {
 		
-		public type: number;
 		public amount: number;
-		
-		constructor(dieType: number, dieAmount: number){
-			this.type = dieType;
+		public type: number;
+				
+		constructor(dieAmount: number, dieType: number ){
 			this.amount = dieAmount;
+			this.type = dieType;			
+		};
+		
+		toString() : String{
+			return (this.amount > 1 ? this.amount : '') + 'w' + this.type;
 		};
 		
 		roll(): number{
 			return 1;
 		};
 	}
-	
-	
+
 	this.getDice = function(step: number): String{
 		return '123';
 	};
@@ -39,6 +67,18 @@ kickleApp.service('stepsService', ['$log', function($log: any){
 	this.rollStep = function(step: number): number{
 		return 3;
 	};
+	
+	function init() {
+		$log.log('initializing stepsService');
+		
+		let s1 = new Step();
+		
+		$log.log('step1: ' + s1.toString());
+		$log.log('rolling step1: ' + s1.roll());
+		
+	};	
+	
+	init();
 	
 }]);
 
