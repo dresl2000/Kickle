5d6bfc14624c3e446c654eaefe56a2f652abc1f0
Andreas Zimmerer <dresl2000@gmx.de>
2017-07-09 10:43:04 +0200

    added mock service

--- KickleFrontend/source/service/attributesService.spec.js
+++ KickleFrontend/source/service/attributesService.spec.js
@@ -2,21 +2,59 @@ describe('attributesService Tests', function() {
 	
 	var attributesService;
 	
-	beforeEach(function() {	  
-		module('kickleApp');
-	});		
 	
+	beforeEach(function() {	  
+		module('kickleApp');				 			
+	});		
+		
+	 beforeEach(function () {
+
+		  mockCharacterDataService = {
+			  getCharacterData: function () {
+				  return {
+							Characters: [ 
+							{
+								Attributes: [
+									{
+										"Type":"DEX",
+										"Value": 11,
+										"Increased": 0
+									},									
+									{
+										"Type":"WIL",
+										"Value": 16,
+										"Increased": 0
+									},
+									{
+										"Type":"CHA",
+										"Value": 18,
+										"Increased": 0
+									}	
+								]
+							}]
+						};
+			  }
+		  };
+
+		  module(function ($provide) {
+			  $provide.value('characterDataService', mockCharacterDataService);
+		  });
+
+	  });			
+		
 	beforeEach(inject(function (_attributesService_) {
 		attributesService = _attributesService_;
 	}));		
-	 
+		
 	it('should have attributesService be defined', function () { 
 		expect(attributesService).toBeDefined();
 	});			
 	     
 	it('should have DEX defined', function () {   
+				
 		let attribute = attributesService.getAttribute('DEX');		
 		
+				
 		expect(attribute.Value).toBe(11); 		
 		expect(attribute.Increased).toBe(0); 
 		expect(attribute.Step).toBe(5); 
