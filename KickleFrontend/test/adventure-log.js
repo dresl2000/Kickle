08dd23be291d8781b8a035ba480e02fd972dc458
Andreas Zimmerer <dresl2000@gmx.de>
2017-08-12 08:32:50 +0200

    end to end tests

--- KickleFrontend/test/adventure-log.js
+++ KickleFrontend/test/adventure-log.js
@@ -3,17 +3,23 @@ describe('adventure-log', function() {
   fit('adventure-log should have logo', function() {
     browser.get('http://localhost:2525/#!/adventure-log');
 
+	
 
-	var imgElement = element(by.css('.logo'));
+	var imgElement = element(by.xpath('//img[@class="logo"]'));
 	expect(imgElement.isPresent()).toBe(true);
+	expect(imgElement.getAttribute("src")).toContain('images/Earthdawn-logo.png');
 	
-	
-
-	//imgElement.src.toEqual('images/Earthdawn-logo.png');
-    //var img = element(by.css('[class="logo"]'));
-    //expect(img.getAttribute('src').toEqual("images/Earthdawn-logo.png"));
+	//using different alternative methods	
+	var method1 = browser.findElement(by.xpath('//div/div/img'));
+	expect(method1.getAttribute("src")).toContain('images/Earthdawn-logo.png');
 		
-
+	var method2 = browser.findElement(by.xpath('//img[@class="logo"]'));
+	expect(method2.getAttribute("src")).toContain('images/Earthdawn-logo.png');
+	
+	var method3 = browser.findElement(by.css('.logo'));
+	expect(method3.getAttribute("src")).toContain('images/Earthdawn-logo.png');	
+	
+	
 
   });
 });
