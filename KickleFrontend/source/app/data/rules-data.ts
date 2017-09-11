8766e96889fec577a4cdeba282a8fc01534593cb
DRESL\dresl <dresl2000@gmx.de>
2017-09-11 07:32:41 +0200

    added mystic armor

--- KickleFrontend/source/app/data/rules-data.ts
+++ KickleFrontend/source/app/data/rules-data.ts
@@ -5,152 +5,182 @@ export const RULESDATA : any = {
 		{
 			"AttributeValue":1,
 			"Step": 2,
-			"Defense": 2
+			"Defense": 2,
+			"MysticArmor" : 0
 		},
 		{
 			"AttributeValue":2,
 			"Step": 2,
-			"Defense": 3
+			"Defense": 3,
+			"MysticArmor" : 0
 		},
 		{
 			"AttributeValue":3,
 			"Step": 2,
-			"Defense": 3
+			"Defense": 3,
+			"MysticArmor" : 0
 		},
 		{
 			"AttributeValue":4,
 			"Step": 3,
-			"Defense": 4
+			"Defense": 4,
+			"MysticArmor" : 0
 		},
 		{
 			"AttributeValue":5,
 			"Step": 3,
-			"Defense": 4
+			"Defense": 4,
+			"MysticArmor" : 0
 		},
 		{
 			"AttributeValue":6,
 			"Step": 3,
-			"Defense": 4
+			"Defense": 4,
+			"MysticArmor" : 0
 		},
 		{
 			"AttributeValue":7,
 			"Step": 4,
-			"Defense": 5
+			"Defense": 5,
+			"MysticArmor" : 0
 		},
 		{
 			"AttributeValue":8,
 			"Step": 4,
-			"Defense": 5
+			"Defense": 5,
+			"MysticArmor" : 0
 		},
 		{
 			"AttributeValue":9,
 			"Step": 4,
-			"Defense": 6
+			"Defense": 6,
+			"MysticArmor" : 0
 		},
 		{
 			"AttributeValue":10,
 			"Step": 5,
-			"Defense": 6
+			"Defense": 6,
+			"MysticArmor" : 0
 		},
 				{
 			"AttributeValue":11,
 			"Step": 5,
-			"Defense": 7
+			"Defense": 7,
+			"MysticArmor" : 1
 		},
 		{
 			"AttributeValue":12,
 			"Step": 5,
-			"Defense": 7
+			"Defense": 7,
+			"MysticArmor" : 1
 		},
 		{
 			"AttributeValue":13,
 			"Step": 6,
-			"Defense": 7
+			"Defense": 7,
+			"MysticArmor" : 1
 		},
 		{
 			"AttributeValue":14,
 			"Step": 6,
-			"Defense": 8
+			"Defense": 8,
+			"MysticArmor" : 2
 		},
 		{
 			"AttributeValue":15,
 			"Step": 6,
-			"Defense": 8
+			"Defense": 8,
+			"MysticArmor" : 2
 		},
 				{
 			"AttributeValue":16,
 			"Step": 7,
-			"Defense": 9
+			"Defense": 9,
+			"MysticArmor" : 2
 		},
 		{
 			"AttributeValue":17,
 			"Step": 7,
-			"Defense": 9
+			"Defense": 9,
+			"MysticArmor" : 3
 		},
 		{
 			"AttributeValue":18,
 			"Step": 7,
-			"Defense": 10
+			"Defense": 10,
+			"MysticArmor" : 3
 		},
 		{
 			"AttributeValue":19,
 			"Step": 8,
-			"Defense": 10
+			"Defense": 10,
+			"MysticArmor" : 3
 		},
 		{
 			"AttributeValue":20,
 			"Step": 8,
-			"Defense": 10
+			"Defense": 10,
+			"MysticArmor" : 4
 		},
 				{
 			"AttributeValue":21,
 			"Step": 8,
-			"Defense": 11
+			"Defense": 11,
+			"MysticArmor" : 4
 		},
 		{
 			"AttributeValue":22,
 			"Step": 9,
-			"Defense": 11
+			"Defense": 11,
+			"MysticArmor" : 4
 		},
 		{
 			"AttributeValue":23,
 			"Step": 9,
-			"Defense": 12
+			"Defense": 12,
+			"MysticArmor" : 5
 		},
 		{
 			"AttributeValue":24,
 			"Step": 9,
-			"Defense": 12
+			"Defense": 12,
+			"MysticArmor" : 5
 		},
 		{
 			"AttributeValue":25,
 			"Step": 10,
-			"Defense": 13
+			"Defense": 13,
+			"MysticArmor" : 5
 		},
 				{
 			"AttributeValue":26,
 			"Step": 10,
-			"Defense": 13
+			"Defense": 13,
+			"MysticArmor" : 6
 		},
 		{
 			"AttributeValue":27,
 			"Step": 10,
-			"Defense": 13
+			"Defense": 13,
+			"MysticArmor" : 6
 		},
 		{
 			"AttributeValue":28,
 			"Step": 11,
-			"Defense": 14
+			"Defense": 14,
+			"MysticArmor" : 6
 		},
 		{
 			"AttributeValue":29,
 			"Step": 11,
-			"Defense": 14
+			"Defense": 14,
+			"MysticArmor" : 7
 		},
 		{
 			"AttributeValue":30,
 			"Step": 11,
-			"Defense": 15
+			"Defense": 15,
+			"MysticArmor" : 7
 		},		
 	]
 	
