ade4e7f1a944d6f0fe5c7083d0e23b3cb3506eb0
DRESL\dresl <dresl2000@gmx.de>
2017-09-16 09:58:15 +0200

    diceboard history

--- KickleFrontend/source/app/components/dice-board/dice-board.component.ts
+++ KickleFrontend/source/app/components/dice-board/dice-board.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy  } from '@angular/core';
+import { Component, OnDestroy, Input  } from '@angular/core';
 import {Subscription  } from 'rxjs/Rx';
 import {DiceRollService} from '../../service/dice-roll.service';
 import { DiceRoll} from '../../shared/diceRoll';
@@ -7,27 +7,43 @@ import { DiceRoll} from '../../shared/diceRoll';
   selector: 'dice-board',
   template: `		
 	<span *ngIf="diceRoll">{{diceRoll.Name}} ({{diceRoll.Dice}}): {{diceRoll.Result}} <span><br/>	
-     <ul>
-      <li *ngFor="let dr of diceRolls | async">
-		{{dr.Name}}: {{dr.Result}}
-      </li>
-    </ul>`
+	
+	<span *ngIf="displayHistory">
+		<h1>History</h1>
+		
+		<ul>
+		  <li *ngFor="let dr of diceRolls | async">
+			{{dr.Name}}: {{dr.Result}}
+		  </li>
+		</ul>		
+		
+	</span>
+	
+    `
 })
 export class DiceBoardComponent { 
 
+	@Input() displayHistory: boolean;
+
 	subscription: Subscription;
 	private diceRoll : DiceRoll;
+	public diceRolls : DiceRoll[];
+	
+	ngOnInit(){
+		this.diceRolls = [];
+		
+	}
 	
 	constructor(private diceRollService : DiceRollService){
 	
-		this.subscription = this.diceRollService.getDiceRoll().subscribe(diceRoll => { console.log( diceRoll.Name);this.diceRoll = diceRoll; });
+		this.subscription = this.diceRollService.getDiceRoll().subscribe(diceRoll => { console.log( diceRoll.Name);this.diceRolls.push(this.diceRoll); this.diceRoll = diceRoll; });
 	
 	}
-	//this.diceRollService.sendDiceRoll(`rolling ${this.name} (${this.stepsService.getDice(this.step)}) ->  ${this.stepsService.rollStep(this.step)}`);
+	
     ngOnDestroy() {
         // unsubscribe to ensure no memory leaks
         this.subscription.unsubscribe();
     }	
 	
-	public diceRolls : DiceRoll[];
+	
 }
