d64610501252d0ab3d97e3fd1a9880be9accfa09
DRESL\dresl <dresl2000@gmx.de>
2017-12-02 18:31:25 +0100

    combat

--- KickleFrontend/source/app/components/combat/combat-main/combat-main.component.ts
+++ KickleFrontend/source/app/components/combat/combat-main/combat-main.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { Action } from '../../../shared/combat/action'
 import {StepsService} from '../../../service/steps.service';
 import { SimpleAction } from '../../../shared/combat/simpleAction';
 import { Combat } from '../../../shared/combat/combat';
-
+import {DiceRollService} from '../../../service/dice-roll.service';
+import {DiceRoll} from '../../../shared/diceRoll';
 
 @Component({
   selector: 'combat-main',
@@ -14,31 +15,42 @@ export class CombatMainComponent {
 	IniActionList : Action[] = [];
 	selectedIniAction : Action;
 
+	IniRoll : DiceRoll;
+
+	AttackActionList : Action[] = [];
+	selectedAttackAction : Action;
+
+	DamageActionList : Action[] = [];
+	selectedDamageAction : Action;
+
 	damage : number;
 	damageInput : number;	
 	combat : Combat;
 
-	constructor(private stepsService: StepsService) { }
+	constructor(private el: ElementRef, private stepsService: StepsService, private diceRollService : DiceRollService ) { 
+
+	}
 	
 	ngOnInit(){
 		console.log('initialzing combat...');
 
 		this.combat = new Combat();
 
-		var sa1 = new SimpleAction(1,"Geschicklichkeit",6,"w10");
-		var sa2 = new SimpleAction(1,"Lufttanz",14,"w20 w4");
-		var sa3 = new SimpleAction(1,"Lufttanz + Tigersprung",18,"w20 w10");
-
-		this.IniActionList.push(new Action(sa1));
-		this.IniActionList.push(new Action(sa2));
-		this.IniActionList.push(new Action(sa3));
-		this.selectedIniAction = this.IniActionList[0];
-
 		this.damage = 11;
 
+		var saIni1 = new SimpleAction(1,"Geschicklichkeit",5,"w8");
+		this.IniActionList.push(new Action(saIni1));
+		this.selectedIniAction = this.IniActionList[0];
+
+		var saAttack1 = new SimpleAction(1,"Spruchzauberei",15,"w20 w6");
+		this.AttackActionList.push(new Action(saAttack1));
+
+		var saAttack2 = new SimpleAction(1,"Fadenweben",15,"w20 w6");
+		this.AttackActionList.push(new Action(saAttack2));
+
+		this.selectedAttackAction = this.AttackActionList[0];		
+
 		//console.log( this.IniActionList.filter(x => x.Id == 1)[0].Name);
-
-
 	}
 
 	beginCombat(){
@@ -53,17 +65,32 @@ export class CombatMainComponent {
 	}
 
 	rollIni(){
+
 		if(!this.selectedIniAction){
 			return;
 		}
-		console.log('rollin Ini: ' +  this.selectedIniAction.Name);
+
+		if(isNaN(this.selectedIniAction.Step))
+		{
+			return;
+		}
 
 		if(this.combat.HasEnded){
 			this.beginCombat();
 		}
 		
+		console.log(`rolling ini: ${this.selectedIniAction.Name}`);		
+		this.IniRoll = new DiceRoll("Initiative",this.selectedIniAction.Step,this.selectedIniAction.Dice, this.stepsService.rollStep(this.selectedIniAction.Step), new Date() );
+
+		this.diceRollService.sendDiceRoll(this.IniRoll);
+
 		this.combat.nextRound();
 	}
+
+	rollAttack(){
+		console.log(`rolling attack: ${this.selectedAttackAction.Name}`);		
+	}
+
 
 	takeDamage(){
 		this.modifyDamage(function(a,b){return a + b;});
